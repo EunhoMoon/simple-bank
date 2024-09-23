a8fd5df08e89f57ae4c6bc37c0c104e4348a6b9e
Janek <eunho.moon@gmail.com>
2024-09-24 00:07:18 +0900

    [feat] init project & init account service

---
+++ accounts/src/test/java/com/motivank/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motivank.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
