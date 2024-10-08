713244bf23d66a205ffd5ccc3231512f2cbb33ca
EunhoMoon <eunho.moon@gmail.com>
2024-10-08 11:46:26 +0900

    [feat] Card 도메인 비지니스 로직 및 테스트 생성

---
+++ cards/src/test/java/com/motivank/cards/repository/CardsRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.motivank.cards.repository;
+
+import com.motivank.cards.entity.Cards;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@SpringBootTest
+class CardsRepositoryTest {
+
+    @Autowired
+    CardsRepository cardsRepository;
+
+    @Test
+    @DisplayName("mobileNumber로 카드 조회시 정상 조회가 가능하다.")
+    void findByMobileNumber() {
+        // given
+        String mobileNumber = "1231234123";
+        Cards newCard = Cards.issuance(mobileNumber);
+        cardsRepository.save(newCard);
+
+        // when
+        var cards = cardsRepository.findByMobileNumber(mobileNumber);
+
+        // then
+        assertThat(cards).isPresent();
+    }
+
+}
