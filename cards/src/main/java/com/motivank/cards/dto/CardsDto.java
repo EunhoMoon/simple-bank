8920fd2291484d283ef736094860f34ac851032c
EunhoMoon <eunho.moon@gmail.com>
2024-10-08 12:19:41 +0900

    [feat] Card 도메인 API 및 Swagger-ui

--- cards/src/main/java/com/motivank/cards/dto/CardsDto.java
+++ cards/src/main/java/com/motivank/cards/dto/CardsDto.java
@@ -1,22 +1,41 @@
 package com.motivank.cards.dto;
 
 import com.motivank.cards.entity.Cards;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Schema(name = "Cards", description = "Schema to hold Card information")
 public class CardsDto {
-
+    @NotEmpty(message = "Mobile number cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should be a 10-digit number")
+    @Schema(description = "Mobile Number of Customer", example = "4354437687")
     private final String mobileNumber;
 
+    @NotEmpty(message = "Card number cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{12})", message = "CardNumber must be 12 digits")
+    @Schema(description = "Card Number", example = "123456789012")
     private final String cardNumber;
 
+    @NotEmpty(message = "Card type cannot be null or empty")
+    @Schema(description = "Type of Card", example = "Credit Card")
     private final String cardType;
 
+    @Positive(message = "Total card limit should be greater than zero")
+    @Schema(description = "Total Limit of Card", example = "10000")
     private final int totalLimit;
 
+    @PositiveOrZero(message = "Total amount used should be equal or greater than zero")
+    @Schema(description = "Amount Used", example = "5000")
     private final int amountUsed;
 
+    @PositiveOrZero(message = "Total available amount should be equal or greater than zero")
+    @Schema(description = "Available Amount", example = "5000")
     private final int availableAmount;
 
     @Builder
