eb25d06574f7ecdd10d3455d71ee0fa6992c0ab4
EunhoMoon <eunho.moon@gmail.com>
2024-10-10 12:24:45 +0900

    [fix] profile 설정 수정

--- accounts/src/main/java/com/motivank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/motivank/accounts/controller/AccountsController.java
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/accounts", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Tag(
-    name = "CRUD REST APIs for Accounts in SimpleBank",
-    description = "CRUD REST APIs for managing customer accounts in SimpleBank"
+        name = "CRUD REST APIs for Accounts in SimpleBank",
+        description = "CRUD REST APIs for managing customer accounts in SimpleBank"
 )
 public class AccountsController {
 
@@ -34,123 +34,123 @@ public class AccountsController {
 
     @PostMapping
     @Operation(
-        summary = "Create a new account",
-        description = "Create a new account with the given customer details"
+            summary = "Create a new account",
+            description = "Create a new account with the given customer details"
     )
     @ApiResponses({
-        @ApiResponse(
-            responseCode = "201",
-            description = "HTTP status code 201 (Created) returned after successfully creating a new account"
-        ),
-        @ApiResponse(
-            responseCode = "500",
-            description = "HTTP status code 500 (Internal Server Error) returned if an error occurred",
-            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
-        )
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP status code 201 (Created) returned after successfully creating a new account"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP status code 500 (Internal Server Error) returned if an error occurred",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            )
     })
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         accountsService.createAccount(customerDto);
 
         return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
     @GetMapping
     @Operation(
-        summary = "Fetch account details",
-        description = "Fetch account details for the given mobile number"
+            summary = "Fetch account details",
+            description = "Fetch account details for the given mobile number"
     )
     @ApiResponses({
-        @ApiResponse(
-            responseCode = "200",
-            description = "HTTP status code 200 (OK) returned after successfully fetching account details"
-        ),
-        @ApiResponse(
-            responseCode = "404",
-            description = "HTTP status code 404 (Not Found) returned if the account details are not found",
-            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
-        ),
-        @ApiResponse(
-            responseCode = "500",
-            description = "HTTP status code 500 (Internal Server Error) returned if an error occurred",
-            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
-        )
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP status code 200 (OK) returned after successfully fetching account details"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP status code 404 (Not Found) returned if the account details are not found",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP status code 500 (Internal Server Error) returned if an error occurred",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            )
     })
     public ResponseEntity<CustomerDto> fetchAccountDetails(
-        @RequestParam
-        @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should be a 10-digit number")
-        String mobileNumber
+            @RequestParam
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should be a 10-digit number")
+            String mobileNumber
     ) {
         return ResponseEntity.status(HttpStatus.OK).body(accountsService.fetchAccountDetails(mobileNumber));
     }
 
     @PutMapping
     @Operation(
-        summary = "Update account details",
-        description = "Update account details for the given customer"
+            summary = "Update account details",
+            description = "Update account details for the given customer"
     )
     @ApiResponses({
-        @ApiResponse(
-            responseCode = "200",
-            description = "HTTP status code 200 (OK) returned after successfully updating account details"
-        ),
-        @ApiResponse(
-            responseCode = "417",
-            description = "HTTP status code 417 (Expectation Failed) returned if the update operation failed",
-            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
-        ),
-        @ApiResponse(
-            responseCode = "500",
-            description = "HTTP status code 500 (Internal Server Error) returned if an error occurred",
-            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
-        )
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP status code 200 (OK) returned after successfully updating account details"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "HTTP status code 417 (Expectation Failed) returned if the update operation failed",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP status code 500 (Internal Server Error) returned if an error occurred",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            )
     })
     public ResponseEntity<ResponseDto> updateAccount(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
 
         return ResponseEntity
-            .status(isUpdated ? HttpStatus.OK : HttpStatus.EXPECTATION_FAILED)
-            .body(new ResponseDto(
-                isUpdated ? AccountsConstants.STATUS_200 : AccountsConstants.STATUS_417,
-                isUpdated ? AccountsConstants.MESSAGE_200 : AccountsConstants.MESSAGE_417_UPDATE
-            ));
+                .status(isUpdated ? HttpStatus.OK : HttpStatus.EXPECTATION_FAILED)
+                .body(new ResponseDto(
+                        isUpdated ? AccountsConstants.STATUS_200 : AccountsConstants.STATUS_417,
+                        isUpdated ? AccountsConstants.MESSAGE_200 : AccountsConstants.MESSAGE_417_UPDATE
+                ));
     }
 
     @DeleteMapping
     @Operation(
-        summary = "Delete account",
-        description = "Delete account for the given mobile number"
+            summary = "Delete account",
+            description = "Delete account for the given mobile number"
     )
     @ApiResponses({
-        @ApiResponse(
-            responseCode = "200",
-            description = "HTTP status code 200 (OK) returned after successfully deleting the account"
-        ),
-        @ApiResponse(
-            responseCode = "417",
-            description = "HTTP status code 417 (Expectation Failed) returned if the delete operation failed",
-            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
-        ),
-        @ApiResponse(
-            responseCode = "500",
-            description = "HTTP status code 500 (Internal Server Error) returned if an error occurred",
-            content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
-        )
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP status code 200 (OK) returned after successfully deleting the account"
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "HTTP status code 417 (Expectation Failed) returned if the delete operation failed",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP status code 500 (Internal Server Error) returned if an error occurred",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            )
     })
     public ResponseEntity<ResponseDto> deleteAccount(
-        @RequestParam
-        @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should be a 10-digit number")
-        String mobileNumber
+            @RequestParam
+            @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should be a 10-digit number")
+            String mobileNumber
     ) {
         boolean isDeleted = accountsService.deleteAccount(mobileNumber);
 
         return ResponseEntity
-            .status(isDeleted ? HttpStatus.OK : HttpStatus.EXPECTATION_FAILED)
-            .body(new ResponseDto(
-                isDeleted ? AccountsConstants.STATUS_200 : AccountsConstants.STATUS_417,
-                isDeleted ? AccountsConstants.MESSAGE_200 : AccountsConstants.MESSAGE_417_DELETE
-            ));
+                .status(isDeleted ? HttpStatus.OK : HttpStatus.EXPECTATION_FAILED)
+                .body(new ResponseDto(
+                        isDeleted ? AccountsConstants.STATUS_200 : AccountsConstants.STATUS_417,
+                        isDeleted ? AccountsConstants.MESSAGE_200 : AccountsConstants.MESSAGE_417_DELETE
+                ));
     }
 
 }
