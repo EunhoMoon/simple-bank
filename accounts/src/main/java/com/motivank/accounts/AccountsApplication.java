79a698fb89ab5533eef6bd255e44606c5590e4bf
Janek <eunho.moon@gmail.com>
2024-10-23 22:30:13 +0900

    [feat] FeignClient interface 생성 및 dto 추가

--- accounts/src/main/java/com/motivank/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/motivank/accounts/AccountsApplication.java
@@ -7,8 +7,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableFeignClients
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
---
+++ accounts/src/main/java/com/motivank/accounts/service/client/CardsFeignClient.java
@@ -0,0 +1,15 @@
+package com.motivank.accounts.service.client;
+
+import com.motivank.accounts.dto.CardsDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "cards")
+public interface CardsFeignClient {
+
+    @GetMapping(value = "/api/cards", consumes = "application/json")
+    ResponseEntity<CardsDto> getCard(@RequestParam String mobileNumber);
+
+}
